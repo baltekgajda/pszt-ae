00f337571a3db0ba4407f8f20c1050cb7bfefe8c
Avinsi <przemwil@gmail.com>
2018-01-04 12:15:22 +0100

    pdf upgrade

--- PlanZajec - PSZT/src/PdfVis/PdfCreator.java
+++ PlanZajec - PSZT/src/PdfVis/PdfCreator.java
@@ -3,6 +3,7 @@ package PdfVis;
 import Evolution.Genotype;
 import inOut.ZarzadzanieDanymi;
 import klasyPodstawowe.Klasa;
+import klasyPodstawowe.Nauczyciel;
 import klasyPodstawowe.Przedmiot;
 import klasyPodstawowe.Timetable;
 import klasyPodstawowe.Zajecia;
@@ -24,8 +25,8 @@ public class PdfCreator {
 	
 
 	private ZarzadzanieDanymi data;
-	
-	
+	String[] start_time = {"8.15","9.15","10.15","11.15","12.15","13.15","14.15","15.15","16.15","17.15","18.15","19.15"};
+	String[] days = {"Pon","Wt","Sr","Czw","Pt"};
 	public PdfCreator(ZarzadzanieDanymi d)
 	{
 		data = d;
@@ -45,10 +46,17 @@ public class PdfCreator {
 		for (ClassTable ct:map.values())
 		{
 			document.add(new Paragraph(ct.k.getNazwa()));
+			
 			table = new Table(Timetable.workingDays+1);
+			
+			table.addCell("");
+			for (int i=0; i<days.length;i++)
+				table.addCell(days[i]);
+			
 			for (int i=0; i<Timetable.workingHours; i++)
 			{
-				table.addCell(Integer.toString(i+1));
+				table.addCell(start_time[i]);
+				
 				for (int j=0; j<Timetable.workingDays; j++)
 				{
 					if (ct.table[j][i]==null)
@@ -59,8 +67,8 @@ public class PdfCreator {
 					{
 						int room = ct.table[j][i].room;
 						Zajecia z = getZajecie(ct.table[j][i].id);
-					
-						String text = z.getPrzedmiot().getNazwa() + "\nSala: " + room;
+						Nauczyciel n = z.getNauczyciel();
+						String text = z.getPrzedmiot().getNazwa() + "\n" + n.getImie().charAt(0) +"." + n.getNazwisko() + "\nsala " + room;
 						table.addCell(text);
 					}
 				}
@@ -132,10 +140,8 @@ public class PdfCreator {
 			}	
 		
 		}
-		
-		
-		return tableMap;
-		
+			
+		return tableMap;	
 	}
 	
 	
@@ -143,7 +149,7 @@ public class PdfCreator {
 	{
 		Zajecia z = data.getArrayZajecia().get(id-1);
 		
-		if (z==null || z.getId()!=id)
+/*		if (z==null || z.getId()!=id)
 		{
 			for (Zajecia zaj:data.getArrayZajecia())
 			{
@@ -152,13 +158,14 @@ public class PdfCreator {
 					break;
 			}
 		}
+*/
 		return z;
 	}
 	
 	private Przedmiot getPrzedmiot(int id)
 	{
 		Przedmiot p = data.getArrayPrzedmiot().get(id-1);
-		/*
+/*
 		if (p==null || p.getId()!=id)
 		{
 			for (Zajecia zaj:data.getArrayZajecia())
@@ -168,7 +175,7 @@ public class PdfCreator {
 					break;
 			}
 		}
-		*/
+*/
 		return p;
 	}
 
